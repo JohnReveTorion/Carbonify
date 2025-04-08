81776c901e3a571ad2753940769124605221062e
JohnReveTorion <johnrevetorion7@gmail.com>
2025-04-08 21:26:32 +0800

    feat: responsiveness

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -14,8 +14,7 @@ const theme = ref('default')
       <v-main>
         <v-container>
           <v-row class="d-flex justify-center pt-5">
-
-            <v-col cols="6" md="6">
+            <v-col cols="12" sm="8" md="6" lg="4">
               <v-card class="mx-auto" prepend-icon="mdi mdi-domain">
                 <template #title>
                   <span class="font-weight-black">CARBONIFY</span>
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -13,8 +13,8 @@ const theme = ref('default')
 
       <v-main>
         <v-container>
-          <v-row class="pt-5">
-            <v-col class="align-center justify-center">
+          <v-row class="align-center justify-center pt-5">
+            <v-col cols="12" sm="8" md="6" lg="4">
               <v-card class="mx-auto" prepend-icon="mdi mdi-domain" subtitle="" width="400">
                 <template v-slot:title>
                   <span class="font-weight-black">CARBONIFY</span>
