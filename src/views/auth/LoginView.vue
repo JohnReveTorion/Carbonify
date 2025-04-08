a8e2344354170d3016c1711f9c2c7ec9179778c1
JohnReveTorion <johnrevetorion7@gmail.com>
2025-04-08 15:53:29 +0800

    added new page

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,17 +1,17 @@
 <script setup>
-import { ref } from 'vue'
+ import { ref } from 'vue'
 
 const theme = ref('light')
 
-function onClick() {
+function onClick () {
   theme.value = theme.value === 'light' ? 'dark' : 'light'
 }
 </script>
 
 <template>
-  <v-responsive class="border rounded">
+     <v-responsive class="border rounded">
     <v-app :theme="theme">
-      <v-app-bar class="px-3">
+      <v-app-bar class="px-3" color="green-darken-3">
         <v-spacer></v-spacer>
 
         <v-btn
@@ -24,9 +24,47 @@ function onClick() {
 
       <v-main>
         <v-container>
-          <h1>Main Content</h1>
+          
+      <v-row class="d-flex justify-center">
+      <v-col cols="12" md="6">
+
+        <v-card
+    class="mx-auto"
+    prepend-icon="mdi mdi-domain"
+    subtitle="Login"
+    
+  >
+    <template v-slot:title>
+      <span class="font-weight-black"  style="color: darkgreen;" >Carbonify</span>
+    </template>
+
+    <v-card-text class="bg-surface-light pt-4">
+      <v-form fast-fail @submit.prevent>
+      <v-text-field label="Company Name"  variant="outlined" ></v-text-field>
+
+      <v-text-field label="Password" variant="outlined" type="password" ></v-text-field>
+
+      <v-btn class="mt-2" rounded="xl" size="large" variant="outlined" block>Log in</v-btn>
+    </v-form>
+     <hr class="my-5">
+   
+
+   <h5>Don't have account? <router-link to="/login"> Click here to register</router-link></h5>
+
+
+
+
+    </v-card-text>
+    
+  </v-card>
+
+      </v-col>
+      </v-row>
+
         </v-container>
       </v-main>
+
+      <v-footer color="green-darken-3" border app> 2024 - Carbonify</v-footer>
     </v-app>
   </v-responsive>
 </template>
---
+++ src/views/auth/RegisterView.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { ref } from 'vue'
+
+const theme = ref('light')
+
+function onClick() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+  <v-responsive class="border rounded">
+    <v-app :theme="theme">
+      <v-app-bar class="px-3" color="green-darken-3">
+        <v-spacer></v-spacer>
+
+        <v-btn
+          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          text="Toggle Theme"
+          slim
+          @click="onClick"
+        ></v-btn>
+      </v-app-bar>
+
+      <v-main>
+        <v-container>
+          <v-row>
+            <v-col class="align-center justify-center">
+              <v-card class="mx-auto" prepend-icon="$vuetify" subtitle="" width="400">
+                <template v-slot:title>
+                  <span class="font-weight-black">CARBONIFY</span>
+                </template>
+
+                <v-card-text class="bg-surface-light pt-4">
+                  <v-form disabled>
+                    <v-text-field v-model="firstName" label="First name"></v-text-field>
+                    <v-text-field v-model="lastName" label="Last name"></v-text-field>
+                    <v-text-field v-model="lastName" label="Email"></v-text-field>
+                    <v-text-field v-model="lastName" label="Phone Number"></v-text-field>
+                  </v-form>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+
+      <v-footer class="align-center justify-center" color="green-darken-3" border app
+        >Copyright Carbonify</v-footer
+      >
+    </v-app>
+  </v-responsive>
+</template>
